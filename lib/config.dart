c2da41839f48d4769bfc5d0a66e5ab2da91f17cb
JaiminBhagat-SmartDome <jaimin.bhagat@outlook.com>
2024-12-15 19:55:47 +0530

    updated base api url

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String apiBaseUrl = "http://localhost:5043/api";
+  static const String apiBaseUrl = "https://smartdome-ins-api-ckhuhyc0gvffa9hr.canadacentral-01.azurewebsites.net/api";
   static const String defaultTenantId = "e5485f45-b90d-44cb-9e05-ae294490d409";
   static const int timeout = 5000; // Timeout in milliseconds
 }
